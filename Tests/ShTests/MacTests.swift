b33502dabea43c307405401d4eff0038a7f47008
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2023-01-28 14:24:33 -0500

    additional tests

--- Tests/ShTests/MacTests.swift
+++ Tests/ShTests/MacTests.swift
@@ -3,25 +3,7 @@ import Sh
 
 #if os(macOS)
 final class MacTests: XCTestCase {
-
-  struct Device: Codable {
-    let availabilityError: String?
-    let dataPath: String
-    let dataPathSize: Int
-    let deviceTypeIdentifier: String
-    let isAvailable: Bool
-    let lastBootedAt: Date?
-    let logPath: String
-    let logPathSize: Int?
-    let name: String
-    let state: String
-    let udid: UUID
-  }
-  
-  struct Devices: Codable {
-    let devices: [String: [Device]]
-  }
-  
+ 
   func testSimctlListDevices() throws {
       let decoder = JSONDecoder()
       decoder.dateDecodingStrategy = .iso8601
@@ -38,6 +20,42 @@ final class MacTests: XCTestCase {
                                decodedBy: decoder,
                                "xcrun simctl list -j devices")
     XCTAssertGreaterThanOrEqual(wrapper.devices.count, 0)
+  }
+  
+  func testSimctlListDevicesAllOutput() throws {
+    let process = Process(cmd: "xcrun simctl list -j devices")
+    let allOutput = try process.runReturningAllOutput()
+    XCTAssertNil(allOutput.terminationError)
+    XCTAssertGreaterThan(allOutput.stdOut.count, 0)
+    XCTAssertTrue(allOutput.stdOut.asTrimmedString()?.localizedCaseInsensitiveContains("com.apple.CoreSimulator") ?? false)
+    XCTAssertTrue(allOutput.stdErr.asTrimmedString()?.localizedCaseInsensitiveContains("Unknown binary with magic") ?? false)
+  }
+  
+  func testSimctlListDevicesAllOutputAsync() async throws {
+    let process = Process(cmd: "xcrun simctl list -j devices")
+    let allOutput = try await process.runReturningAllOutput()
+    XCTAssertNil(allOutput.terminationError)
+    XCTAssertGreaterThan(allOutput.stdOut.count, 0)
+    XCTAssertTrue(allOutput.stdOut.asTrimmedString()?.localizedCaseInsensitiveContains("com.apple.CoreSimulator") ?? false)
+    XCTAssertTrue(allOutput.stdErr.asTrimmedString()?.localizedCaseInsensitiveContains("Unknown binary with magic") ?? false)
+  }
+  
+  private struct Device: Codable {
+    let availabilityError: String?
+    let dataPath: String
+    let dataPathSize: Int
+    let deviceTypeIdentifier: String
+    let isAvailable: Bool
+    let lastBootedAt: Date?
+    let logPath: String
+    let logPathSize: Int?
+    let name: String
+    let state: String
+    let udid: UUID
+  }
+  
+  private struct Devices: Codable {
+    let devices: [String: [Device]]
   }
 }
 #endif
