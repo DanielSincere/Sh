83da64ab499144a8b32a8d5feddebfe6f3df193f
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-07-09 21:40:20 -0400

    ensure we’re not in the middle of a read when stopping

--- Sources/Sh/Data/PipeBuffer.swift
+++ Sources/Sh/Data/PipeBuffer.swift
@@ -8,6 +8,8 @@ class PipeBuffer {
   let pipe = Pipe()
   private var buffer: Data = .init()
   private let queue: DispatchQueue
+
+  private let semaphore = DispatchGroup()
   
   convenience init(id: StreamID, qos: DispatchQoS.QoSClass = .userInitiated) {
     self.init(label: id.rawValue, qos: qos)
@@ -21,16 +23,21 @@ class PipeBuffer {
     )
 
     self.pipe.fileHandleForReading.readabilityHandler = { handler in
+
       self.queue.async {
+        self.semaphore.enter()
         let data = try! handler.readToEnd()
         self.buffer.append(contentsOf: data ?? Data())
+        self.semaphore.leave()
       }
     }
   }
 
   func closeReturningData() -> Data {
+
     let data = queue.sync {
-      self.buffer
+      self.semaphore.wait()
+      return self.buffer
     }
 
     self.pipe.fileHandleForReading.readabilityHandler = nil
