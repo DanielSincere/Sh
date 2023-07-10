ccc4aa80554450f8ddf1b19a76d952c61b509279
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-07-09 22:23:52 -0400

    cleanup?

--- Sources/Sh/Data/PipeBuffer.swift
+++ Sources/Sh/Data/PipeBuffer.swift
@@ -5,23 +5,13 @@ class PipeBuffer {
     case stdOut, stdErr
   }
   
-  let pipe = Pipe()
+  internal let pipe = Pipe()
   private var buffer: Data = .init()
-  private let queue: DispatchQueue
-
   private let semaphore = DispatchGroup()
-  private let lock = NSLock()
-  
-  convenience init(id: StreamID, qos: DispatchQoS.QoSClass = .userInitiated) {
-    self.init(label: id.rawValue, qos: qos)
-  }
-  
-  init(label: String, qos: DispatchQoS.QoSClass = .userInitiated) {
 
-    self.queue = DispatchQueue(
-      label: label,
-      target: DispatchQueue.global(qos: qos)
-    )
+  let id: StreamID
+  init(id: StreamID) {
+    self.id = id
 
     self.pipe.fileHandleForReading.readabilityHandler = { handler in
       self.semaphore.enter()
@@ -32,20 +22,12 @@ class PipeBuffer {
   }
 
   func closeReturningData() -> Data {
-    let result = self.semaphore.wait(timeout: .now() + 1.1)
-    switch result {
-    case .success:
-      print("SUCCESS")
-    case .timedOut:
-      print("TIME OUT")
-    }
+    self.semaphore.wait()
 
-    return queue.sync {
-      self.semaphore.wait()
-      let data = self.buffer
-      self.buffer = Data()
-      self.pipe.fileHandleForReading.readabilityHandler = nil
-      return data
-    }
+    self.semaphore.wait()
+    let data = self.buffer
+    self.buffer = Data()
+    self.pipe.fileHandleForReading.readabilityHandler = nil
+    return data
   }
 }
