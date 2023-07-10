bc35d2e82d8edb5a538e61be55b779feca013cd4
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-07-09 22:29:39 -0400

    de-dupe?

--- Tests/ShTests/AsyncTests.swift
+++ Tests/ShTests/AsyncTests.swift
@@ -13,24 +13,6 @@ final class AsyncTests: XCTestCase {
     XCTAssertNil(allOutput.terminationError)
   }
 
-  func testCustomDecodeJsonOutputAsync() async throws {
-    let json = #"'[{"type":"start","date":"2022-10-29T19:22:22Z"},{"type":"stop","date":"2023-10-29T19:22:22Z"}]'"#
-
-    struct Event: Decodable {
-      let type: String
-      let date: Date
-    }
-
-    let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .iso8601
-    let events = try await sh([Event].self, decodedBy: decoder, "echo \(json)")
-    XCTAssertEqual(events.count, 2)
-    XCTAssertEqual(events.first?.type, "start")
-    XCTAssertEqual(events.first?.date.timeIntervalSince1970, 1667071342)
-    XCTAssertEqual(events.last?.type, "stop")
-    XCTAssertEqual(events.last?.date.timeIntervalSince1970, 1698607342)
-  }
-
   func testPrintingErrorWhenFileOutputIsLong() throws {
     do {
       try sh(.file("/tmp/sh-AsyncTests.testPrintingErrorWhenFileOutputIsLong.log"), """
--- Tests/ShTests/LogFileTests.swift
+++ Tests/ShTests/LogFileTests.swift
@@ -3,92 +3,70 @@ import Foundation
 @testable import Sh
 
 final class LogFileTests: XCTestCase {
-
+  
   func testSimple() throws {
     try sh(.file("/tmp/sh-LogFileTests.testSimple.log"),
            #"echo "simple""#)
     XCTAssertEqual(try String(contentsOfFile: "/tmp/sh-LogFileTests.testSimple.log"), "simple\n")
   }
-
+  
   func testSimpleAsync() async throws {
     try await sh(.file("/tmp/sh-LogFileTests.testSimpleAsync.log"),
                  #"echo "simple""#)
     XCTAssertEqual(try String(contentsOfFile: "/tmp/sh-LogFileTests.testSimpleAsync.log"), "simple\n")
   }
-
-   func testPrintingErrorWhenFileOutputIsShort() throws {
-     do {
-       try sh(.file("/tmp/sh-LogFileTests.testPrintingErrorWhenFileOutputIsShort.log"),
-              #"echo "simple" > /unknown/path/name"#)
-       XCTFail("Expected the above to throw an `Errors.errorWithLogInfo`")
-     } catch Errors.errorWithLogInfo(let logInfo, underlyingError: let underlyingError) {
-
-       XCTAssertTrue(logInfo.contains("/unknown/path/name"))
-
-       let terminationError = try XCTUnwrap(underlyingError as? TerminationError)
-
-       XCTAssertNotEqual(terminationError.status, 0)
-       XCTAssertEqual(terminationError.reason, .exit)
-
-       let error = Errors.errorWithLogInfo(logInfo, underlyingError: underlyingError)
-       XCTAssertTrue(error.localizedDescription.contains("/unknown/path/name"))
-     } catch {
-       XCTFail("Expected the above to throw an `Errors.errorWithLogInfo`, instead got an \(error)")
-     }
-   }
-
-  func testPrintingErrorWhenFileOutputIsLong() throws {
+  
+  func testPrintingErrorWhenFileOutputIsShort() throws {
     do {
-      try sh(.file("/tmp/sh-LogFileTests.testPrintingErrorWhenFileOutputIsLong.log"),
-             "swift test --package-path Fixtures/SwiftProjectWithFailingTests")
+      try sh(.file("/tmp/sh-LogFileTests.testPrintingErrorWhenFileOutputIsShort.log"),
+             #"echo "simple" > /unknown/path/name"#)
       XCTFail("Expected the above to throw an `Errors.errorWithLogInfo`")
     } catch Errors.errorWithLogInfo(let logInfo, underlyingError: let underlyingError) {
-
-      XCTAssertTrue(logInfo.contains(#"XCTAssertEqual failed: ("Some name") is not equal to ("Wrong name")"#))
-
+      
+      XCTAssertTrue(logInfo.contains("/unknown/path/name"))
+      
       let terminationError = try XCTUnwrap(underlyingError as? TerminationError)
-
+      
       XCTAssertNotEqual(terminationError.status, 0)
       XCTAssertEqual(terminationError.reason, .exit)
-
+      
       let error = Errors.errorWithLogInfo(logInfo, underlyingError: underlyingError)
-      XCTAssertTrue(error.localizedDescription.contains(#"XCTAssertEqual failed: ("Some name") is not equal to ("Wrong name")"#))
-
+      XCTAssertTrue(error.localizedDescription.contains("/unknown/path/name"))
     } catch {
       XCTFail("Expected the above to throw an `Errors.errorWithLogInfo`, instead got an \(error)")
     }
   }
-
+  
   func testCreatesMissingLogfiles() throws {
-
+    
     do {
       try sh(.file("/tmp/missing/path/sh-testCreatesMissingLogfiles.log"),
              #"echo "simple" > /unknown/path/name"#)
     } catch Errors.errorWithLogInfo(let info, underlyingError: let underlyingError) {
-      #if os(Linux)
+#if os(Linux)
       XCTAssertEqual(info, "/bin/sh: 1: cannot create /unknown/path/name: Directory nonexistent")
       XCTAssertEqual(underlyingError.localizedDescription, "Exited with code 2.")
-      #else
+#else
       XCTAssertEqual(info, "/bin/sh: /unknown/path/name: No such file or directory")
       XCTAssertEqual(underlyingError.localizedDescription, "Exited with code 1.")
-      #endif
+#endif
     } catch {
       XCTFail("Expected an errorWithLogInfo, but got \(error)")
     }
   }
-
+  
   func testUnwritableLogfile() throws {
     do {
       try sh(.file("/missing/path/sh-test.log"),
              #"echo "simple" > /unknown/path/name"#)
     } catch Errors.openingLogError(let logError, underlyingError: let underlyingError) {
-
+      
       XCTAssertEqual(logError.localizedDescription, "Exited with code 1.")
-      #if os(Linux)
+#if os(Linux)
       XCTAssertEqual(underlyingError.localizedDescription, "The operation could not be completed. You don’t have permission.")
-      #else
+#else
       XCTAssertEqual(underlyingError.localizedDescription, "You can’t save the file “path” because the volume is read only.")
-      #endif
+#endif
     } catch {
       XCTFail("Expected an opening log error, but got \(error)")
     }
