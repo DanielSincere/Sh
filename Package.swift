71114003020d6c4713cd75c723495f95c06d440b
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-07-10 01:39:15 -0400

    remove macos-12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "Sh",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
   ],
   products: [
     .library(name: "Sh", targets: ["Sh"]),
